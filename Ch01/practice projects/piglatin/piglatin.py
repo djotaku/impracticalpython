cccc73f90498edcfc496e9ebf7cfd1873e07be50
Eric Mesa <ericsbinaryworld@gmail.com>
2019-09-15 15:57:19 -0400

    vowel pig latinization is done. But, then again, that was the easy one.

--- Ch01/practice projects/piglatin/piglatin.py
+++ Ch01/practice projects/piglatin/piglatin.py
@@ -1,10 +1,10 @@
 """Takes in a word from the user and returns it in Pig Latin."""
  
 def main():
-    word = input("Give me a word! ")
+    word = input("Give me a word! \n\n")
     vowels = ['a','e','i','o','u']
     if word[0].lower() in vowels:
-        print("vowel")
+        print(f"\n\nPig latin: {word}way")
          
 if __name__ == "__main__":
     main()

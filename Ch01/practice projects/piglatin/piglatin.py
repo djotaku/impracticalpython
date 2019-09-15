4a37cbb60e8340590ee88c093437c002b9d61b54
Eric Mesa <ericsbinaryworld@gmail.com>
2019-09-15 19:37:36 -0400

    now piglatin.py passes PEP8 - apparently space required after commas. Just like in English!

--- Ch01/practice projects/piglatin/piglatin.py
+++ Ch01/practice projects/piglatin/piglatin.py
@@ -1,13 +1,13 @@
 """Takes in a word from the user and returns it in Pig Latin."""
- 
+
 def main():
     """Convert the word to Pig Latin."""
     word = input("Give me a word! \n\n")
-    vowels = ['a','e','i','o','u']
+    vowels = ['a', 'e', 'i', 'o', 'u']
     if word[0].lower() in vowels:
         print(f"\n\nPig latin: {word}way")
     else:
         print(f"\n\nPig latin: {word[1:]}{word[0]}ay")
-         
+
 if __name__ == "__main__":
     main()

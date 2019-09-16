67352b55e69945488e388ed83871e28a816db89b
Eric Mesa <ericsbinaryworld@gmail.com>
2019-09-15 20:32:34 -0400

    I've got it counting correctly, but not creating the bar chart

---
+++ Ch01/practice projects/ETAOIN/ETAOIN.py
@@ -0,0 +1,18 @@
+"""Takes in a sentence and creates a Poor Man's Bar Chart."""
+from collections import defaultdict
+import pprint
+
+def main():
+    """Run the code to create the bar chart"""
+    sentence = input("Give me a sentence to analyze: ")
+    pp = pprint.PrettyPrinter(indent=4)
+    d = defaultdict(int)
+    for letter in sentence.lower():
+        if letter == " ":
+            pass
+        else:
+            d[letter] += 1
+    pp.pprint(d)
+
+if __name__ == "__main__":
+    main()

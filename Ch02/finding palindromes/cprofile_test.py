0ae39ad2c25bbac56928673c12fc2381d3aa2e9b
Eric Mesa <ericsbinaryworld@gmail.com>
2019-12-20 20:18:22 -0500

    to profile the palingram code

---
+++ Ch02/finding palindromes/cprofile_test.py
@@ -0,0 +1,7 @@
+import cProfile
+import palingrams
+cProfile.run('palingrams.find_palingrams()')
+
+# note from book: 
+# The most common way to run cProfile is directly in the interpreter.
+# This lets you dump your output to a text file and view it with a web viewer

3d877fa878c5147efdfc049f17541777c278e2a8
Eric Mesa <ericsbinaryworld@gmail.com>
2020-02-13 10:52:09 -0500

    finished typing the voldemort_british

---
+++ Ch03/finding voldemort/load_dictionary.py
@@ -0,0 +1,28 @@
+""" Load a text file as a list.
+
+Arguments:
+-text file name (and directory path, if needed)
+
+Exceptions:
+-IOError if filename not found.
+
+Returns:
+-A list of all words in a text file in lower case.
+
+Requires:
+-import sys
+
+"""
+
+import sys
+
+def load(file):
+    """Open a text file & return a list of lowercase strings."""
+    try:
+        with open(file) as in_file:
+            loaded_text = in_file.read().strip().split('\n')
+            loaded_text = [x.lower() for x in loaded_text]
+            return loaded_text
+    except IOError as e:
+        print(f"{e}\nError opening {file}.".format(file=sys.stderr))
+        sys.exit(1)

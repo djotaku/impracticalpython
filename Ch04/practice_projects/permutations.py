19c1fdcee0a720e991e3ab5eacc318ae9d8f2bb0
Eric Mesa <ericsbinaryworld@gmail.com>
2020-12-28 20:41:48 -0500

    OK, everything is now hunky-dory for the brute-force
    Just had to fix that permutation, as I figured.

--- Ch04/practice_projects/permutations.py
+++ Ch04/practice_projects/permutations.py
@@ -1,13 +1,14 @@
 """Return a collection of tuples of all possible permutations for cipher keys."""
 
-from itertools import combinations
+from itertools import permutations, product
 
 def key_permutations(columns):
-    values_for_key = []
-    for number in range(-columns, columns):
-        if number != 0:
-            values_for_key.append(number)
-    return permutations(values_for_key, r=columns)
+    values_for_key = [number for number in range(1, columns+1)]
+    return_values = []
+    for permutation in permutations(values_for_key):
+        for signs in product([-1,1], repeat=len(values_for_key)):
+            return_values.append([number*sign for number, sign in zip(permutation, signs)])
+    return return_values
 
 def main():
     columns = int(input("How many columns will your route cipher have? "))
